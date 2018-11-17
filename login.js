a692b8f733b19027789968bfe61c4b294405e9d2
jack <jackcrawford2008@gmail.com>
2018-11-16 19:06:52 -0500

    post function and login function

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-$(document).on('click', '#loginbutton', function(){
+$(document).on('click', '#submitbutton', function(){
    var user = document.getElementById("username").value;
    sessionStorage.user = user;
    window.location = "TBD";
