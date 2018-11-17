847d7db1bfee8201873102a07d3342eae4c45ba3
jack <jackcrawford2008@gmail.com>
2018-11-17 01:15:39 -0500

    Changes

--- backend.php
+++ backend.php
@@ -83,9 +83,16 @@ function upvote($connect){
   $ip = mysqli_real_escape_string($connect, $_POST['ip_address']);
   $post_id = mysqli_real_escape_string($connect, $_POST['post_id']);
 
-  $query = "SELECT COUNT(*) FROM banned_ips WHERE ip = '$ip'";
+  $query = "SELECT COUNT(*) FROM updown WHERE ip = '$ip' and post_id = '$post_id';";
   $result = mysqli_fetch_array(mysqli_query($connect, $query));
   $results = $result['COUNT(*)'];
+  if ($results > 1){
+    $query = "UPDATE updown SET up_or_down = 'u' WHERE ip = '$ip' and post_id = '$post_id';";
+    $run_query = mysqli_query($connect, $query);
+  } else {
+    $query = "INSERT INTO updown (ip, post_id, up_or_down) VALUES ('$ip', '$post_id', 'u');";
+    $run_query = mysqli_query($connect, $query);
+  }
 }
 $method = $_GET['method'];
 if (!empty($method)){
--- home.js
+++ home.js
@@ -11,29 +11,32 @@ $( document ).ready(function() {
 });
 $(document).on('click', '#postButton', function() {
   var postVal = document.getElementById("captionText").value;
-  var user = sessionStorage.user;
-  getIPAddress();
-  console.log(postVal + " " + user + " " + " " + longii + " " + latii)
-  var formData = new FormData();
-  formData.append('username', user);
-  formData.append('content', postVal);
-  formData.append('long', longii);
-  formData.append('lat', latii);
-  $.ajax({
-      url: "/cobweb/backend.php?method=post_post&long="+longii+"&lat="+latii, // Url to which the request is send
-      type: "POST",
-      data: formData, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
-      contentType: false,       // The content type used when sending data to the server.
-      cache: false,             // To unable request pages to be cached
-      processData:false,        // To send DOMDocument or non processed data file it is set to false
-      success: function(data){
-        console.log("succ" + data);
-        showPosition(navigator.geolocation.getCurrentPosition(showPosition));
-      },
-      error: function(data) {
-           console.log(data);
+  if(postVal()) {
+
+    var user = sessionStorage.user;
+    getIPAddress();
+    console.log(postVal + " " + user + " " + " " + longii + " " + latii)
+    var formData = new FormData();
+    formData.append('username', user);
+    formData.append('content', postVal);
+    formData.append('long', longii);
+    formData.append('lat', latii);
+    $.ajax({
+        url: "/cobweb/backend.php?method=post_post&long="+longii+"&lat="+latii, // Url to which the request is send
+        type: "POST",
+        data: formData, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+        contentType: false,       // The content type used when sending data to the server.
+        cache: false,             // To unable request pages to be cached
+        processData:false,        // To send DOMDocument or non processed data file it is set to false
+        success: function(data){
+          console.log("succ" + data);
+          showPosition(navigator.geolocation.getCurrentPosition(showPosition));
         },
-    });
+        error: function(data) {
+             console.log(data);
+          },
+      });
+    }
 });
 var ip_address;
 var getIPAddress = function() {
